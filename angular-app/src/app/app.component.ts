d6793611d5d84cd7ca3d8eba65cd015e8bead2df
Sachin Giri <sachidanandgiri23@gmail.com>
2022-05-29 23:23:09 +0530

    I have done the functionality to fetch list of public repositories and apply search and sort on the repo name. I have also made the name clickable to go ti that browser

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-app';
+  userName:String="";
+  userProfilePic:String="";
+  userRepo:any;
+  term:any;
+  order: string = 'name';
+  reverse: boolean = false;
+
+  constructor(private http:HttpClient){
+  }
+  
+  ngOnInit(){
+    this.http.get(environment.gitRepoUrl).subscribe((data) => this.setUserRepoData(data));
+  }
+  
+  setUserRepoData(data:any){
+    this.userRepo = data;
+    this.userName = data[0].owner.login;
+    this.userProfilePic = data[0].owner.avatar_url;
+    console.log("Name", this.userName);
+    console.log("Pic", this.userProfilePic); 
+    console.log("MyProfile", this.userRepo);
+  }
+
+  setOrder(order:any) {
+    this.order = order;
+    this.reverse = !this.reverse;
+  }
 }
--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from "@angular/common/http";
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { FormsModule } from '@angular/forms';
+import { OrderModule } from 'ngx-order-pipe';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    Ng2SearchPipeModule,
+    FormsModule,
+    OrderModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
